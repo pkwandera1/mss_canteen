83b5143620657fedc526d20dcff3eeebfb420195
pkwandera1 <pkwandera1@gmail.cm>
2025-04-14 22:30:10 +0300

    Add page loader in the html

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // Clears dist folder before each build
   },
- module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-   ],
- },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Your custom HTML template
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
